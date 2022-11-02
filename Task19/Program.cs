1aaa0e1abe29d4fc5a6100f1be0cda6cc7b3bdbe
leonid ivanov <leonid.ivanov.w@gmail.com>
2022-11-03 03:39:13 +0500

    Task 19 done

---
+++ Task19/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Enter a 5-digit number:");
+int num = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("The number is: " + num + ". Is it a palindrome? --> " + IfPalindrome(num));
+bool IfPalindrome(int number)
+{
+    int a = (number / 1000) / 10;
+    int b = (number / 1000) % 10;
+    int z = (number % 100) % 10;
+    int y = (number % 100) / 10;
+    if (a == z && b == y) return true;
+    else return false;
+}
